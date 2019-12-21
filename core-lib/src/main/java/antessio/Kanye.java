2cda86cf8b4cb03bc11f347f860b89a969fdb96f
Antonio D'Alessio <antonio.dalessio@satispay.com>
2019-12-21 11:50:58 +0100

    Initial import core lib

---
+++ core-lib/src/main/java/antessio/Kanye.java
@@ -0,0 +1,20 @@
+package antessio;
+
+public class Kanye {
+    private String quote;
+
+    public Kanye(String quote) {
+        this.quote = quote;
+    }
+
+    public Kanye() {
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
